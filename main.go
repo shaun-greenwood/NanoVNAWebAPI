3b9f4f127d31b4a995295f8b36099c8661384275
shaun-greenwood <shaun@greenwoodhome.net>
2024-09-14 21:00:48 +0100

    had a go at listing serial ports

--- main.go
+++ main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"go.bug.st/serial"
 )
 
 func main(){
-	fmt.Println("hello, world.")
+	//list available serial ports
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//if there weren't any serial ports available, throw a wobbly
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found")
+	}
+
+	//if you found some ports, spit it out
+	for _, port := range ports {
+		fmt.Printf("Found port: %v\n", port)
+	}
 }
